a031febffe1e5a08c2123c5024743016efff95d3
zzangae <zzangae@gmail.com>
2017-04-06 07:20:25 +0900

    BlockFocusAttack - 1.0 Original version 0.37

---
+++ BlockFocusAttack/BlockFocusAttack.cs
@@ -0,0 +1,49 @@
+﻿using ModAPI.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BlockFocusAttack
+{
+    public class BlockFocusAttack : MonoBehaviour
+    {
+        public const string ModName = "BlockFocusAttack";
+
+        public static int LastFocus
+        {
+            get;
+            set;
+        }
+
+        public static bool ShouldAttack
+        {
+            get
+            {
+                return Environment.TickCount - BlockFocusAttack.LastFocus > 200;
+            }
+            set
+            {
+                if (value)
+                {
+                    BlockFocusAttack.LastFocus = 0;
+                }
+            }
+        }
+
+        [ExecuteOnGameStart]
+        private static void Init()
+        {
+            new GameObject(string.Format("__{0}__", "BlockFocusAttack")).AddComponent<BlockFocusAttack>();
+        }
+
+        private void OnApplicationFocus(bool focusStatus)
+        {
+            if (focusStatus)
+            {
+                BlockFocusAttack.LastFocus = Environment.TickCount;
+            }
+        }
+    }
+}
