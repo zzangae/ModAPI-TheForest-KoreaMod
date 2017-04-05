245b28a8d55a51b406c6a2329f26ed93cbe0421f
zzangae <zzangae@gmail.com>
2017-04-06 08:34:38 +0900

    GUI.skin 에서 UnityEngine.GUI,  로 수정

--- InventoryMod/Inventory.cs
+++ InventoryMod/Inventory.cs
@@ -1,6 +1,10 @@
-﻿using UnityEngine;
+﻿using ModAPI;
+using ModAPI.Attributes;
+using System;
 using TheForest.Items;
+using TheForest.Items.Craft;
 using TheForest.Utils;
+using UnityEngine;
 
 namespace InventoryMod
 {
@@ -25,36 +29,36 @@ namespace InventoryMod
         {
             if (this.visible)
             {
-                GUI.skin = ModAPI.GUI.Skin;
+                UnityEngine.GUI.skin = ModAPI.GUI.Skin;
 
-                Matrix4x4 Matrix = GUI.matrix;
+                Matrix4x4 Matrix = UnityEngine.GUI.matrix;
 
                 if (labelStyle == null)
                 {
-                    labelStyle = new GUIStyle(GUI.skin.label);
+                    labelStyle = new GUIStyle(UnityEngine.GUI.skin.label);
                     labelStyle.fontSize = 12;
                 }
-                
-                GUI.Box(new Rect(10, 10, 400, 450), "Cheat menu", GUI.skin.window);
-                scrollPosition = GUI.BeginScrollView(new Rect(10, 50, 390, 350), scrollPosition, new Rect(0, 0, 350, cY));
+
+                UnityEngine.GUI.Box(new Rect(10, 10, 400, 450), "Cheat menu", UnityEngine.GUI.skin.window);
+                scrollPosition = UnityEngine.GUI.BeginScrollView(new Rect(10, 50, 390, 350), scrollPosition, new Rect(0, 0, 350, cY));
                 this.cY = 25f;
                 for (int i = 0; i < ItemDatabase.Items.Length; ++i)
                 {
-                    GUI.Label(new Rect(20f, cY, 150f, 20f), ItemDatabase.Items[i]._name, labelStyle);
-                    if(GUI.Button(new Rect(170f, cY, 150f, 20f), "Add"))
+                    UnityEngine.GUI.Label(new Rect(20f, cY, 150f, 20f), ItemDatabase.Items[i]._name, labelStyle);
+                    if(UnityEngine.GUI.Button(new Rect(170f, cY, 150f, 20f), "Add"))
                     {
                         LocalPlayer.Inventory.AddItem(ItemDatabase.Items[i]._id, 1, false, false, (WeaponStatUpgrade.Types)(-2), 0f);
                     }
                     this.cY += 30f;
                 }
-                GUI.EndScrollView();
+                UnityEngine.GUI.EndScrollView();
                 
-                if(GUI.Button(new Rect(20f, 410f, 100f, 20f), "Close"))
+                if(UnityEngine.GUI.Button(new Rect(20f, 410f, 100f, 20f), "Close"))
                 {
                     this.visible = false;
                 }
 
-                GUI.matrix = Matrix;
+                UnityEngine.GUI.matrix = Matrix;
             }
         }
 
