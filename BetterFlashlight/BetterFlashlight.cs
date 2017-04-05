1d1056ad3b1a4d68b03d36691bc8b9f48e08c561
zzangae <zzangae@gmail.com>
2017-04-05 21:35:13 +0900

    BetterFlashlight 1.0.0.1 - 0.57c

--- BetterFlashlight/BetterFlashlight.cs
+++ BetterFlashlight/BetterFlashlight.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bolt;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +13,11 @@ namespace BetterFlashlight
     {
         protected override void Update()
         {
-            if (BoltNetwork.get_isRunning() && (!BoltNetwork.get_isRunning() || !base.get_entity().get_isAttached() || !base.get_entity().get_isOwner()))
+            if (BoltNetwork.isRunning && (!BoltNetwork.isRunning || !base.entity.isAttached || !base.entity.isOwner))
             {
                 return;
-            }
-            LocalPlayer.Stats.BatteryCharge -= this._batterieCostPerSecond * Time.get_deltaTime();
+            }            
+            LocalPlayer.Stats.BatteryCharge -= this._batterieCostPerSecond * Time.deltaTime;
             if ((double)LocalPlayer.Stats.BatteryCharge > 20.0)
             {
                 this.SetIntensity(0.85f);
@@ -46,28 +47,28 @@ namespace BetterFlashlight
                     this.TorchLowerLight();
                 }
             }
-            if (!BoltNetwork.get_isRunning())
+            if (!BoltNetwork.isRunning)
             {
                 return;
             }
-            base.get_state().set_BatteryTorchIntensity(this._mainLight.get_intensity());
-            base.get_state().set_BatteryTorchEnabled(this._mainLight.get_enabled());
-            base.get_state().set_BatteryTorchColor(this._mainLight.get_color());
+            base.state.BatteryTorchIntensity = this._mainLight.intensity;
+            base.state.BatteryTorchEnabled = this._mainLight.enabled;
+            base.state.BatteryTorchColor = this._mainLight.color;
         }
 
         protected override void TorchLowerLight()
         {
-            this.SetIntensity(Random.Range(0.6f, 0.45f));
+            this.SetIntensity(UnityEngine.Random.Range(0.6f, 0.45f));
         }
 
         protected override void TorchLowerLightMore()
         {
-            this.SetIntensity(Random.Range(0.4f, 0.2f));
+            this.SetIntensity(UnityEngine.Random.Range(0.4f, 0.2f));
         }
 
         protected override void TorchLowerLightEvenMore()
         {
-            this.SetIntensity(Random.Range(0.2f, 0.03f));
+            this.SetIntensity(UnityEngine.Random.Range(0.2f, 0.03f));
         }
     }
 }
