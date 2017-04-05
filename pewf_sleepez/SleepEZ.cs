f7bba6cb908ee7eaf7d89e266567cbb5c9368fbd
zzangae <zzangae@gmail.com>
2017-04-06 07:43:51 +0900

    SleepEZ - 0.1.0 Original version 0.45b

---
+++ pewf_sleepez/SleepEZ.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheForest.UI;
+using TheForest.Utils;
+
+namespace pewf_sleepez
+{
+    internal class SleepEZ : SleepTimerDisplay
+    {
+        protected override void Update()
+        {
+            if (LocalPlayer.Stats)
+            {
+                LocalPlayer.Stats.NextSleepTime = 0f;
+                this._fillSprite.fillAmount = 1f;
+            }
+        }
+    }
+}
