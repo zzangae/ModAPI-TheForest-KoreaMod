c7f8b103fcd873b3c761afe1aa41d7c9bfebb086
zzangae <zzangae@gmail.com>
2017-04-06 20:25:04 +0900

    Full Inventory - 1.0.0.1 game version 0.54b

---
+++ FullInventory/FillInv.cs
@@ -0,0 +1,47 @@
+﻿using ModAPI;
+using ModAPI.Attributes;
+using System;
+using TheForest.Items;
+using TheForest.Items.Craft;
+using TheForest.Utils;
+using TheForest.Items.Inventory;
+using UnityEngine;
+
+namespace FullInventory
+{
+    public class FillInv : MonoBehaviour
+    {
+        private static string GOName = "__FillInv__";
+
+        [ExecuteOnGameStart]
+        private static void PutThisOnScene()
+        {
+            if (!BoltNetwork.isClient)
+            {
+                new GameObject(FillInv.GOName).AddComponent<FillInv>();
+            }
+        }
+
+        private void Update()
+        {
+            if (ModAPI.Input.GetButtonDown("FillInventory"))
+            {
+                for (int i = 0; i < ItemDatabase.Items.Length; i++)
+                {
+                    Item item = ItemDatabase.Items[i];
+                    try
+                    {
+                        if (item._maxAmount >= 0)
+                        {                            
+                            LocalPlayer.Inventory.AddItem(item._id, 100000, true, false, null);
+
+                        }
+                    }
+                    catch (System.Exception)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
