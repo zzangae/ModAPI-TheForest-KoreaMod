9686fe7ed828669f0055917bebe41fce07e28369
zzangae <zzangae@gmail.com>
2017-04-06 20:39:03 +0900

    maxarrows100 1.0.0.0 game version 0.39

---
+++ maxarrows/Moar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheForest.Items;
+using ModAPI.Attributes;
+using UnityEngine;
+
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+namespace maxarrows
+{
+    internal class Moar : MonoBehaviour
+    {
+        [ExecuteOnGameStart]
+        private static void Iamacheater()
+        {
+            for (int i = 0; i < ItemDatabase.Items.Length; i++)
+            {
+                if (ItemDatabase.Items[i]._name.Equals("Arrows"))
+                {
+                    ItemDatabase.Items[i]._maxAmount = 500;
+                    return;
+                }
+            }
+        }
+    }
+}
