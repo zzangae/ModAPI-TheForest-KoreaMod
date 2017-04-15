3c788c1f0c9c9ce558444ccf21cb12eef3bb4f0a
zzangae <zzangae@gmail.com>
2017-04-15 12:22:20 +0900

    Blueprint remove 1.0.0.0

---
+++ BluePrint_Remove/BluePrintRemove.cs
@@ -0,0 +1,38 @@
+﻿using ModAPI.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheForest.Utils;
+using UnityEngine;
+using TheForest.Buildings.Creation;
+
+namespace BluePrint_Remove
+{
+    class BluePrintRemove : MonoBehaviour
+    {
+
+        [ExecuteOnGameStart]
+        private static void AddMeToScene()
+        {
+            new GameObject("__BluePrint__").AddComponent<BluePrintRemove>();
+        }       
+
+        private void Update()
+        {
+            if (ModAPI.Input.GetButtonDown("BluePrint_Remove"))
+            {
+                Craft_Structure[] array = UnityEngine.Object.FindObjectsOfType<Craft_Structure>();
+                if (array != null && array.Length > 0)
+                {
+                    Craft_Structure[] array2 = array;
+                    for (int i = 0; i < array2.Length; i++)
+                    {
+                        Craft_Structure craft_Structure = array2[i];
+                        craft_Structure.CancelBlueprint();
+                    }
+                }
+            }
+        }
+    }
+}
