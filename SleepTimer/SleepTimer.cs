d55ab1af489a0e23c7c860f7fbaa47b4cb884789
zzangae <zzangae@gmail.com>
2017-09-26 13:58:59 +0900

    SleepTimer - 1.1.67.1 (0.67c)

--- SleepTimer/SleepTimer.cs
+++ SleepTimer/SleepTimer.cs
@@ -15,22 +15,41 @@ namespace SleepTimer
         {
             if (ModAPI.Input.GetButtonDown("SleepTimer"))
             {
-                if (!BoltNetwork.isClient && Scene.SceneTracker.allPlayers.Count >= 1)
+                if (!BoltNetwork.isClient || SteamClientDSConfig.isDSFirstClient)
                 {
+                    if (Scene.SceneTracker.allPlayers.Count >= 1)
+                    {                        
+                        Transform transform2 = this.mutantControl.findClosestEnemy(transform);
+                        if (transform2 && !LocalPlayer.IsInCaves && (LocalPlayer.ScriptSetup.targetFunctions.visibleEnemies.Count > 0 || Vector3.Distance(transform.position, transform2.transform.position) < 65f))
+                        {
+                            GraphNode node = AstarPath.active.GetNearest(transform2.transform.position, NNConstraint.Default).node;
+                            uint area = node.Area;
+                            NNConstraint nNConstraint = new NNConstraint();
+                            nNConstraint.constrainArea = true;
+                            int area2 = (int)area;
+                            nNConstraint.area = area2;
+                            GraphNode node2 = AstarPath.active.GetNearest(transform.position, nNConstraint).node;
+                            Vector3 a = new Vector3((float)(node2.position[0] / 1000), (float)(node2.position[1] / 1000), (float)(node2.position[2] / 1000));
+                            if (Vector3.Distance(a, LocalPlayer.Transform.position) < 6f)
+                            {
+                                base.StartCoroutine("setupSleepEncounter", transform2.gameObject);
+                                this.GoToSleepFake();                                
+                                return;
+                            }
+                        }
+                    }
                     Scene.MutantSpawnManager.offsetSleepAmounts();
                     Scene.MutantControler.startSetupFamilies();
                     EventRegistry.Player.Publish(TfEvent.Slept, null);
-                    base.NextSleepTime = Scene.Clock.ElapsedGameTime;
-                    this.Invoke("TurnOffSleepCam", 3f);
-                    this.Tired = 0f;
-                    this.Atmos.TimeLapse();
-                    Scene.HudGui.GuiCam.SetActive(false);
-                    Scene.Cams.SleepCam.SetActive(true);
-                    this.Energy += 100f;
-                    return;
                 }
-                base.Wake();
-            }
+                this.Invoke("TurnOffSleepCam", 3f);
+                this.Tired = 0f;
+                this.Atmos.TimeLapse();
+                Scene.HudGui.ToggleAllHud(false);
+                Scene.Cams.SleepCam.SetActive(true);
+                this.Energy += 100f; 
+                return;
+            }            
             base.Update();
         }
     }
