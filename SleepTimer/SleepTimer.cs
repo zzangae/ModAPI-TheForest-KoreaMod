b0bed59329f61dc387305303f559c7c62b64463b
zzangae <zzangae@gmail.com>
2017-09-26 13:03:21 +0900

    SleepTimer - 1.67.1 (0.67c)

---
+++ SleepTimer/SleepTimer.cs
@@ -0,0 +1,37 @@
+﻿using Pathfinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheForest.Tools;
+using TheForest.Utils;
+using UnityEngine;
+
+namespace SleepTimer
+{
+    internal class SleepTimer : PlayerStats
+    {
+        protected override void Update()
+        {
+            if (ModAPI.Input.GetButtonDown("SleepTimer"))
+            {
+                if (!BoltNetwork.isClient && Scene.SceneTracker.allPlayers.Count >= 1)
+                {
+                    Scene.MutantSpawnManager.offsetSleepAmounts();
+                    Scene.MutantControler.startSetupFamilies();
+                    EventRegistry.Player.Publish(TfEvent.Slept, null);
+                    base.NextSleepTime = Scene.Clock.ElapsedGameTime;
+                    this.Invoke("TurnOffSleepCam", 3f);
+                    this.Tired = 0f;
+                    this.Atmos.TimeLapse();
+                    Scene.HudGui.GuiCam.SetActive(false);
+                    Scene.Cams.SleepCam.SetActive(true);
+                    this.Energy += 100f;
+                    return;
+                }
+                base.Wake();
+            }
+            base.Update();
+        }
+    }
+}
