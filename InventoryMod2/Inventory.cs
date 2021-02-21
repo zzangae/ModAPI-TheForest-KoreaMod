1437f49bf0bab31fca375d67cd3022b45cc9e8d4
zzangae <zzangae@gmail.com>
2021-02-21 20:12:17 +0900

    Inventory Mod 2 - 1.0.73.7 - v1.11b

--- InventoryMod2/Inventory.cs
+++ InventoryMod2/Inventory.cs
@@ -1,6 +1,7 @@
 ﻿using ModAPI.Attributes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TheForest.Items;
 using TheForest.Items.Craft;
 using TheForest.Utils;
@@ -10,15 +11,16 @@ namespace InventoryMod2
 {
     internal class Inventory : MonoBehaviour
     {
-        public Vector2 scrollPosition1 = Vector2.zero;
-        public Vector2 scrollPosition2 = Vector2.zero;
-        public Vector2 scrollPosition3 = Vector2.zero;
-        public Vector2 scrollPosition4 = Vector2.zero;
-        public Vector2 scrollPosition5 = Vector2.zero;
+        public Vector2 scPoWeapons = Vector2.zero;
+        public Vector2 scPoItems = Vector2.zero;
+        public Vector2 scPoRes = Vector2.zero;
+        public Vector2 scPoAnimalHeads = Vector2.zero;
+        public Vector2 scPoOther = Vector2.zero;
+        public Vector2 scPoAll = Vector2.zero;
         private List<Item> listWeapons = new List<Item>();
         private List<Item> listItems = new List<Item>();
         private List<Item> listResources = new List<Item>();
-        private List<Item> listAnimals = new List<Item>();
+        private List<Item> listAnimalHeads = new List<Item>();
         private List<Item> listOther = new List<Item>();
         private string[] catWeapons = new string[22]
         {
@@ -133,6 +135,7 @@ namespace InventoryMod2
         private float cY2;
         private float cY3;
         private float cY4;
+        private float cY5;
         private bool sorted;
         private bool skip;
         private string buffer;
@@ -166,7 +169,7 @@ namespace InventoryMod2
                     {
                         if (this.buffer.Contains(catAnimal))
                         {
-                            this.listAnimals.Add(ItemDatabase.Items[index]);
+                            this.listAnimalHeads.Add(ItemDatabase.Items[index]);
                             this.skip = true;
                             break;
                         }
@@ -204,7 +207,7 @@ namespace InventoryMod2
             this.listWeapons.Sort((Comparison<Item>)((x, y) => string.Compare(x._name, y._name)));
             this.listItems.Sort((Comparison<Item>)((x, y) => string.Compare(x._name, y._name)));
             this.listResources.Sort((Comparison<Item>)((x, y) => string.Compare(x._name, y._name)));
-            this.listAnimals.Sort((Comparison<Item>)((x, y) => string.Compare(x._name, y._name)));
+            this.listAnimalHeads.Sort((Comparison<Item>)((x, y) => string.Compare(x._name, y._name)));
             this.listOther.Sort((Comparison<Item>)((x, y) => string.Compare(x._name, y._name)));
         }
 
@@ -227,35 +230,16 @@ namespace InventoryMod2
                 {
                     new GUIContent("Weapons"),
                     new GUIContent("Items"),
-                    new GUIContent("Resources"),
+                    new GUIContent("Res."),
                     new GUIContent("Head"),
-                    new GUIContent("Other")
+                    new GUIContent("Other"),
+                    new GUIContent("All")
                 }, UnityEngine.GUI.skin.GetStyle("Tabs"));
             if (this.Tab == 0)
             {
-                this.scrollPosition1 = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scrollPosition1, new Rect(0.0f, 0.0f, 330f, this.cY1));
+                this.scPoWeapons = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scPoWeapons, new Rect(0.0f, 0.0f, 330f, this.cY1));
                 this.cY1 = 25f;
-                GUI.Label(new Rect(20f, this.cY1, 150f, 20f), "All Items", this.labelStyle);
-                if (GUI.Button(new Rect(210f, this.cY1, 70f, 20f), "Give all"))
-                {
-                    for (int i = 0; i < ItemDatabase.Items.Length; i++)
-                    {
-                        Item item = ItemDatabase.Items[i];
-                        try
-                        {
-                            if (item._maxAmount >= 0)
-                            {
-                                LocalPlayer.Inventory.AddItem(item._id, 1000 - LocalPlayer.Inventory.AmountOf(item._id, true), true, false, null);
-                            }
-                        }
-                        catch (Exception)
-                        { }
-                    }
-                }
-                this.cY1 = this.cY1 + 30f;
-                GUI.Label(new Rect(20f, this.cY1, 150f, 20f), "[ Weapons ]", this.labelStyle);
-                this.cY1 = this.cY1 + 30f;
-
+                //Weapons
                 for (int index1 = 0; index1 < this.listWeapons.Count; ++index1)
                 {
                     GUI.Label(new Rect(20f, this.cY1, 150f, 20f), this.listWeapons[index1]._name, this.labelStyle);
@@ -283,11 +267,9 @@ namespace InventoryMod2
             }
             if (this.Tab == 1)
             {
-                this.scrollPosition2 = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scrollPosition2, new Rect(0.0f, 0.0f, 330f, this.cY2));
+                this.scPoItems = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scPoItems, new Rect(0.0f, 0.0f, 330f, this.cY2));
                 this.cY2 = 25f;
-                GUI.Label(new Rect(20f, this.cY2, 150f, 20f), "[ Items ]", this.labelStyle);
-                this.cY2 = this.cY2 + 30f;
-
+                //Items
                 for (int index = 0; index < this.listItems.Count; ++index)
                 {
                     GUI.Label(new Rect(20f, this.cY2, 150f, 20f), this.listItems[index]._name, this.labelStyle);
@@ -301,10 +283,9 @@ namespace InventoryMod2
             }            
             if (this.Tab == 2)
             {
-                this.scrollPosition3 = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scrollPosition3, new Rect(0.0f, 0.0f, 330f, this.cY3));
+                this.scPoRes = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scPoRes, new Rect(0.0f, 0.0f, 330f, this.cY3));
                 this.cY3 = 25f;
-                GUI.Label(new Rect(20f, this.cY3, 150f, 20f), "[ Resources ]", this.labelStyle);
-                this.cY3 = this.cY3 + 30f;
+                //Resource
                 for (int index1 = 0; index1 < this.listResources.Count; ++index1)
                 {
                     GUI.Label(new Rect(20f, this.cY3, 150f, 20f), this.listResources[index1]._name, this.labelStyle);
@@ -325,16 +306,15 @@ namespace InventoryMod2
             }
             if (this.Tab == 3)
             {
-                this.scrollPosition4 = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scrollPosition4, new Rect(0.0f, 0.0f, 330f, this.cY4));
+                this.scPoAnimalHeads = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scPoAnimalHeads, new Rect(0.0f, 0.0f, 330f, this.cY4));
                 this.cY4 = 25f;
-                GUI.Label(new Rect(20f, this.cY4, 150f, 20f), "[ Heads ]", this.labelStyle);
-                this.cY4 = this.cY4 + 30f;
-                for (int index = 0; index < this.listAnimals.Count; ++index)
+                //AnimalHeads
+                for (int index = 0; index < this.listAnimalHeads.Count; ++index)
                 {
-                    GUI.Label(new Rect(20f, this.cY4, 150f, 20f), this.listAnimals[index]._name, this.labelStyle);
+                    GUI.Label(new Rect(20f, this.cY4, 150f, 20f), this.listAnimalHeads[index]._name, this.labelStyle);
                     if (GUI.Button(new Rect(210f, this.cY4, 150f, 20f), "Add"))
                     {
-                        LocalPlayer.Inventory.AddItem(this.listAnimals[index]._id, 1, false, false, null);
+                        LocalPlayer.Inventory.AddItem(this.listAnimalHeads[index]._id, 1, false, false, null);
                     }
                     this.cY4 = this.cY4 + 30f;
                 }
@@ -342,10 +322,9 @@ namespace InventoryMod2
             }
             if (this.Tab == 4)
             {
-                this.scrollPosition5 = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scrollPosition5, new Rect(0.0f, 0.0f, 330f, this.cY4));
+                this.scPoOther = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scPoOther, new Rect(0.0f, 0.0f, 330f, this.cY4));
                 this.cY4 = 25f;
-                GUI.Label(new Rect(20f, this.cY4, 150f, 20f), "[ Other ]", this.labelStyle);
-                this.cY4 = this.cY4 + 30f;
+                //Other
                 for (int index = 0; index < this.listOther.Count; ++index)
                 {
                     GUI.Label(new Rect(20f, this.cY4, 150f, 20f), this.listOther[index]._name, this.labelStyle);
@@ -355,6 +334,41 @@ namespace InventoryMod2
                     }
                     this.cY4 = this.cY4 + 30f;
                 }                
+                GUI.EndScrollView();
+            }
+            if (this.Tab ==5)
+            {
+                this.scPoAll = GUI.BeginScrollView(new Rect(5f, 45f, 390f, 370f), this.scPoAll, new Rect(0.0f, 0.0f, 330f, this.cY5));
+                this.cY5 = 25f;
+                //All Items
+                GUI.Label(new Rect(20f, this.cY5, 150f, 20f), "[ All Items ]", this.labelStyle);
+                if (GUI.Button(new Rect(100f, this.cY5, 70f, 20f), "Give all"))
+                {
+                    for (int i = 0; i < ItemDatabase.Items.Length; i++)
+                    {
+                        Item item = ItemDatabase.Items[i];
+                        try
+                        {
+                            if (item._maxAmount >= 0)
+                            {
+                                LocalPlayer.Inventory.AddItem(item._id, 1000 - LocalPlayer.Inventory.AmountOf(item._id, true), true, false, null);
+                            }
+                        }
+                        catch (Exception)
+                        { }
+                    }
+                }
+
+                this.cY5 = this.cY5 + 35f;
+                //list Items
+                GUI.Label(new Rect(20f, this.cY5, 150f, 20f), "[ List Items - item number ]", this.labelStyle);
+                this.cY5 = 55f;
+                foreach (Item item in ItemDatabase.Items.OrderBy((Item i) => i._name))
+                {
+                    this.cY5 = this.cY5 + 30f;
+                    string text = item._name + " [" + item._id + "]";
+                    GUI.Label(new Rect(20f, this.cY5, 150f, 20f), text, this.labelStyle);
+                }
                 GUI.EndScrollView();
                 GUI.matrix = matrix;
             }
